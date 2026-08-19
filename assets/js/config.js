1063196174016ab8d9683d49271092a9d5652417
Luiz Henrique <luuizmorais@gmail.com>
2026-08-18 21:39:58 -0300

    chore: permitir rastreamento de config.js para conexao do supabase no netlify

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+// CONFIGURAÇÃO DO SUPABASE
+// Este arquivo é local e NÃO deve ser enviado para o repositório Git!
+const SUPABASE_URL = 'https://mfxjhsszkywwkpqfuepp.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_uMlijmLuk7X_3NfD1bsPyQ_Eos7A9t2';
